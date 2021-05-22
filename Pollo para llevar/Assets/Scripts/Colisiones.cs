21bce718219c71b93c50da518aaf86e1b0709f9f
Miranda Miranda <miranda.miranda@potros.itson.edu.mx>
2021-05-22 15:05:21 -0700

    Actualización enemigos

--- Pollo para llevar/Assets/Scripts/Colisiones.cs
+++ Pollo para llevar/Assets/Scripts/Colisiones.cs
@@ -8,7 +8,7 @@ using SpeechLib;
 public class Colisiones : MonoBehaviour {
 	public GameObject vidaTotal;
 	private int puntos=0;
-	private int vida=0;
+	private int vida=5;
 	public AudioClip SonidoCoin;
 
 	private SpVoice voice;
@@ -39,5 +39,16 @@ public class Colisiones : MonoBehaviour {
 		if (objeto.gameObject.tag=="Toad"){
 			voice.Speak("Encuentra y recolecta 20 monedas para comprar un carrito", SpeechVoiceSpeakFlags.SVSFlagsAsync);
 		}
+		if (objeto.gameObject.tag== "chickenSpace") {
+			Debug.Log ("Toque enemigo");
+			//gameObject.GetComponent<AudioSource> ().PlayOneShot (SonidoCoin, 0.7f);
+			//Destroy (objeto.gameObject);
+			vida -= 3;
+			vidaTotal.GetComponent<Text>().text=vida.ToString();
+		}
+		if(vida <= 0){
+			Over.show();
+		}
+
 	}
 }
