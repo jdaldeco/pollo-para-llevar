7474804fdc980fbbb0c1afa5f89e47928efa4174
Félix Romero José Francisco <51935664+p4n5ho@users.noreply.github.com>
2021-05-23 22:50:10 -0700

    Soy un pendejo 2.0

---
+++ Pollo para llevar/Assets/Scripts/Enemigo_Movimiento2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo_Movimiento2 : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goToTarget();
+    }
+
+    private void goToTarget() {
+        agent.SetDestination(target.transform.position);
+    }
+}
